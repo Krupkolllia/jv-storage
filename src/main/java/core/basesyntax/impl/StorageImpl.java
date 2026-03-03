f3723c97774e21bd59bbf24cc5dd40e947b9a97c
Krupkolllia <krupkoilla397@gmail.com>
2026-03-03 19:06:21 +0100

    Implement StorageImpl class

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,58 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int STORAGE_SIZE = 10;
+    private static final Object EMPTY = new Object();
+    private final K[] keys;
+    private final V[] values;
+    private int currentSize = -1;
+
+    @SuppressWarnings("unchecked")
+    public StorageImpl() {
+        keys = (K[]) new Object[STORAGE_SIZE];
+        values = (V[]) new Object[STORAGE_SIZE];
+        Arrays.fill(keys, EMPTY);
+        Arrays.fill(values, null);
+    }
+
     @Override
     public void put(K key, V value) {
+        int keyIndex = findKeyIndex(key);
+
+        if (keyIndex != -1) {
+            keys[keyIndex] = key;
+            values[keyIndex] = value;
+            return;
+        }
+
+        currentSize++;
+        keys[currentSize] = key;
+        values[currentSize] = value;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int keyIndex = findKeyIndex(key);
+        if (keyIndex == -1) {
+            return null;
+        }
+        return values[keyIndex];
     }
 
     @Override
     public int size() {
+        return currentSize + 1;
+    }
+
+    private int findKeyIndex(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if (key == keys[i] || (keys[i] != null && keys[i].equals(key))) {
+                return i;
+            }
+        }
         return -1;
     }
 }
